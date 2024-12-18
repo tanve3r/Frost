fb980705f65db56efe8aa366895f7fd456c8be0d
Tanveer <tanve3r@gmail.com>
2024-12-18 19:14:29 +0530

    fixed ble connection issue

--- app_modules/device_drivers/ble/src/ble.c
+++ app_modules/device_drivers/ble/src/ble.c
@@ -126,6 +126,7 @@ static int ble_gap_event(struct ble_gap_event *event, void *arg)
     // Advertise again after completion of the event
     case BLE_GAP_EVENT_DISCONNECT:
         ESP_LOGI("GAP", "BLE GAP EVENT DISCONNECTED");
+        ble_app_advertise();
         break;
     case BLE_GAP_EVENT_ADV_COMPLETE:
         ESP_LOGI("GAP", "BLE GAP EVENT");
